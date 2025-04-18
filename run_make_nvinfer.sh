d74e8e6be240db7a262ec14261a277adb5f2f196
VolodymyrRatushnyi <volodymyr.ratushnyi@logivations.com>
2025-04-18 18:14:53 +0300

    RTDT-3453 Label Studio: fix configs for models

--- run_make_nvinfer.sh
+++ run_make_nvinfer.sh
@@ -1,21 +1,49 @@
 #!/bin/bash
 
-MODEL_DIR=$1
-MODEL_DIR=${MODEL_DIR%%/}
-shift
+MODEL_DIR=""
+CLASSES=()
+RES=()
 
-CLASSES=( "$@" )
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+        --model-dir)
+            MODEL_DIR="$2"
+            shift 2
+            ;;
+        --classes)
+            shift
+            while [[ $# -gt 0 ]] && [[ ! "$1" =~ ^-- ]]; do
+                CLASSES+=("$1")
+                shift
+            done
+            ;;
+        --res)
+            shift
+            while [[ $# -gt 0 ]] && [[ ! "$1" =~ ^-- ]]; do
+                RES+=("$1")
+                shift
+            done
+            ;;
+        *)
+            echo "Unknown parameter: $1"
+            exit 1
+            ;;
+    esac
+done
+
+MODEL_DIR=${MODEL_DIR%%/}
 CLASSES=$(IFS=';' ; echo "${CLASSES[*]}")
+RES=$(IFS=';' ; echo "${RES[*]}")
 
 echo "[property]
 onnx-file=pvt_detector.onnx
-
-# model config
 infer-dims=3;384;512
-gie-unique-id=1
 
 [custom]
+# 1 - PVT, 2 - DEIM, 3 - TAO
 detector-type=1
+min_confidence = 0.5
+res=$RES
 labels=$CLASSES
 report-labels=$CLASSES
 " > "$MODEL_DIR/object-config.txt"
