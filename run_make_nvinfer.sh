48d5aa9bb6b5f5961ca74ea512af6173070b5de2
VolodymyrRatushnyi <volodymyr.ratushnyi@logivations.com>
2025-04-18 17:18:30 +0300

    RTDT-3453 Label Studio: fix configs for models

--- run_make_nvinfer.sh
+++ run_make_nvinfer.sh
@@ -9,16 +9,13 @@ CLASSES=$(IFS=';' ; echo "${CLASSES[*]}")
 
 echo "[property]
 onnx-file=pvt_detector.onnx
-model-engine-file=pvt_detector.onnx_b1_gpu0_fp16.engine
 
 # model config
 infer-dims=3;384;512
 gie-unique-id=1
-detector-type=RetinaNet
 
 [custom]
+detector-type=1
 labels=$CLASSES
 report-labels=$CLASSES
 " > "$MODEL_DIR/object-config.txt"
-
-
