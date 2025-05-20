86fc2b6c4128b216a94d06bd28e62d7eab50a585
VolodymyrRatushnyi <volodymyr.ratushnyi@logivations.com>
2025-05-20 17:28:46 +0300

    nvinfer-cmd-params

--- run_make_nvinfer.sh
+++ run_make_nvinfer.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
-MODEL_DIR=""
+NVINFER_FILE=""
+ONNX_FILENAME=""
 CLASSES=()
 RES=()
 
 while [[ $# -gt 0 ]]; do
     case "$1" in
-        --model-dir)
-            MODEL_DIR="$2"
+        --nvinfer-file)
+            NVINFER_FILE="$2"
+            shift 2
+            ;;
+        --onnx-filename)
+            ONNX_FILENAME="$2"
             shift 2
             ;;
         --classes)
@@ -31,12 +36,11 @@ while [[ $# -gt 0 ]]; do
     esac
 done
 
-MODEL_DIR=${MODEL_DIR%%/}
 CLASSES=$(IFS=';' ; echo "${CLASSES[*]}")
 RES=$(IFS=';' ; echo "${RES[*]}")
 
 echo "[property]
-onnx-file=pvt_detector.onnx
+onnx-file=$ONNX_FILENAME
 infer-dims=3;$RES
 
 [custom]
@@ -45,4 +49,4 @@ detector-type=1
 min_confidence = 0.5
 labels=$CLASSES
 report-labels=$CLASSES
-" > "$MODEL_DIR/object-config.txt"
+" > "$NVINFER_FILE"
